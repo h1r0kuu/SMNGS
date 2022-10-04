394270d0a2c866048243448ef79d487c0a4f836e
h1r0kuu <abhazim.nurbat@gmail.com>
2022-10-04 20:25:27 +0200

    rename

--- src/main/java/com/smnas/backend/controller/SubjectController.java
+++ src/main/java/com/smnas/backend/controller/SubjectController.java
@@ -17,31 +17,31 @@ public class SubjectController {
     private final SubjectMapper subjectMapper;
 
     @GetMapping
-    private ResponseEntity<List<SubjectResponse>> getAllSubjects() {
+    private ResponseEntity<List<SubjectResponse>> getAll() {
         List<SubjectResponse> subjects = subjectMapper.getAll();
         return ResponseEntity.ok(subjects);
     }
 
     @PostMapping
-    private ResponseEntity<SubjectResponse> createSubject(@RequestBody SubjectRequest subjectRequest) {
+    private ResponseEntity<SubjectResponse> create(@RequestBody SubjectRequest subjectRequest) {
         SubjectResponse newSubject = subjectMapper.create(subjectRequest);
         return ResponseEntity.ok(newSubject);
     }
 
     @GetMapping("/{subjectId}")
-    private ResponseEntity<SubjectResponse> getSubjectById(@PathVariable("subjectId") Long subjectId) {
+    private ResponseEntity<SubjectResponse> getById(@PathVariable("subjectId") Long subjectId) {
         SubjectResponse subject = subjectMapper.getById(subjectId);
         return ResponseEntity.ok(subject);
     }
 
     @DeleteMapping("/{subjectId}")
-    private ResponseEntity<String> deleteSubjectById(@PathVariable("subjectId") Long subjectId) {
+    private ResponseEntity<String> deleteById(@PathVariable("subjectId") Long subjectId) {
         subjectMapper.deleteById(subjectId);
         return ResponseEntity.ok("Subject successfully deleted");
     }
 
     @PatchMapping("/{subjectId}")
-    private ResponseEntity<SubjectResponse> changeSubjectById(@PathVariable("subjectId") Long subjectId, @RequestBody SubjectRequest newSubject) {
+    private ResponseEntity<SubjectResponse> changeById(@PathVariable("subjectId") Long subjectId, @RequestBody SubjectRequest newSubject) {
         SubjectResponse subject = subjectMapper.changeById(subjectId, newSubject);
         return ResponseEntity.ok(subject);
     }
