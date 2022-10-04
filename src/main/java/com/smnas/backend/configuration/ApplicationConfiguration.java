26834d38f454140c040ff63a96f71c2d74c44f42
h1r0kuu <abhazim.nurbat@gmail.com>
2022-10-04 19:27:25 +0200

    Password encoder

--- src/main/java/com/smnas/backend/configuration/ApplicationConfiguration.java
+++ src/main/java/com/smnas/backend/configuration/ApplicationConfiguration.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ApplicationConfiguration {
@@ -14,5 +16,10 @@ public class ApplicationConfiguration {
         modelMapper.getConfiguration()
                 .setMatchingStrategy(MatchingStrategies.STRICT);
         return modelMapper;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
