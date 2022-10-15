4c83f27b8ee91540b48f30689209be7abdbf4052
h1r0kuu <abhazim.nurbat@gmail.com>
2022-10-15 20:47:36 +0200

    update

--- src/main/java/com/smnas/backend/dto/schedule/ScheduleResponse.java
+++ src/main/java/com/smnas/backend/dto/schedule/ScheduleResponse.java
@@ -1,4 +1,21 @@
 package com.smnas.backend.dto.schedule;
 
+import com.smnas.backend.dto.group.GroupResponse;
+import com.smnas.backend.dto.subject.SubjectResponse;
+import com.smnas.backend.entity.Group;
+import com.smnas.backend.entity.Subject;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
 public class ScheduleResponse {
+    private Long id;
+    private GroupResponse group;
+    private SubjectResponse subject;
+    private LocalDateTime timeStart;
+    private LocalDateTime timeEnd;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
