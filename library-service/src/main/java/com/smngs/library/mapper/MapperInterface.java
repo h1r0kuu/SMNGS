f228778d7b17bfdf68e4d519ff13efb054b8ee82
h1r0kuu <abhazim.nurbat@gmail.com>
2022-12-05 18:40:53 +0100

    Mapper interface

---
+++ library-service/src/main/java/com/smngs/library/mapper/MapperInterface.java
@@ -0,0 +1,16 @@
+package com.smngs.library.mapper;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface MapperInterface<T, S> {
+    S create(T t);
+    List<S> findAll();
+    Page<S> findAll(Pageable pageable);
+    S findById(Long id);
+    void deleteById(Long id);
+    void deleteAllById(List<Long> ids);
+    S update(T t);
+}
