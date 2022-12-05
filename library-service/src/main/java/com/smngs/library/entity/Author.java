57a688959561b4b6089bf40f0ce7a87ae2dd991c
h1r0kuu <abhazim.nurbat@gmail.com>
2022-12-05 18:39:12 +0100

    Author entity

--- library-service/src/main/java/com/smngs/library/entity/Author.java
+++ library-service/src/main/java/com/smngs/library/entity/Author.java
@@ -2,11 +2,14 @@ package com.smngs.library.entity;
 
 
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
-@Table(name = "category")
+@Table(name = "author")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -17,4 +20,22 @@ public class Author {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    @ToString.Exclude
+    private List<Book> books;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @CreationTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
