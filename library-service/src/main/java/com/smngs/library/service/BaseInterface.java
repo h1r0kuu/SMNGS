429e606fcaa558147fa1942e9a6194be05d8f7c4
h1r0kuu <abhazim.nurbat@gmail.com>
2022-12-05 18:39:42 +0100

    Service & mapper

---
+++ library-service/src/main/java/com/smngs/library/service/BaseInterface.java
@@ -0,0 +1,16 @@
+package com.smngs.library.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface BaseInterface<T> {
+    T create(T t);
+    List<T> findAll();
+    Page<T> findAll(Pageable pageable);
+    T findById(Long id);
+    void deleteById(Long id);
+    void deleteAllById(List<Long> ids);
+    T update(T t);
+}
