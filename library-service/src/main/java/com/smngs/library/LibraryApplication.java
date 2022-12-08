bda532dcd91072b0e156447b5e844730a6bdf689
h1r0kuu <abhazim.nurbat@gmail.com>
2022-12-08 17:36:06 +0100

    Mapper conf

--- library-service/src/main/java/com/smngs/library/LibraryApplication.java
+++ library-service/src/main/java/com/smngs/library/LibraryApplication.java
@@ -1,8 +1,11 @@
 package com.smngs.library;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,4 +14,13 @@ public class LibraryApplication {
         SpringApplication.run(LibraryApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper mapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setAmbiguityIgnored(true)
+                .setSkipNullEnabled(true)
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
 }
