3f04f5f663ae982a74e47b0d97a2db54f6b43ea9
h1r0kuu <abhazim.nurbat@gmail.com>
2022-12-20 14:57:00 +0100

    Fix number that displaying the count of displayed items

--- client/src/components/Pagination/Pagination.tsx
+++ client/src/components/Pagination/Pagination.tsx
@@ -8,7 +8,6 @@ interface Props {
 }
 
 const Pagination: FC<Props> = ({pagination}): ReactElement => {
-    console.log(pagination)
     const hasPrevious = () => {
         return pagination.pageable.pageNumber !== 0
     }
@@ -19,11 +18,25 @@ const Pagination: FC<Props> = ({pagination}): ReactElement => {
         return pagination.pageable.pageNumber + 1 < pagination.totalPages
     }
 
+    const showingFrom = () => {
+        if(pagination.pageable.pageNumber === 0) {
+            return 1
+        }
+        return pagination.pageable.pageNumber * pagination.size
+    }
+
+    const showingTo = () => {
+        if(pagination.pageable.pageNumber + 1 === pagination.totalPages) {
+            return pagination.totalElements
+        }
+        return (pagination.pageable.pageNumber + 1) * pagination.size
+    }
+
     return (
         <Row style={{alignItems: "center", marginTop: "20px"}}>
             <Col sm={12} md={5}>
                 <div className="dataTables_info" id="DataTables_Table_0_info" role="status"
-                     aria-live="polite">Showing {pagination.size * ((pagination.pageable.pageNumber + 1) - 1)} to {(pagination.pageable.pageNumber + 1) * pagination.size} of {pagination.totalElements} entries
+                     aria-live="polite">Showing {showingFrom()} to  {showingTo()} of {pagination.totalElements} entries
                 </div>
             </Col>
             <Col sm={12} md={7}>
@@ -32,22 +45,22 @@ const Pagination: FC<Props> = ({pagination}): ReactElement => {
                     <ul className="pagination">
                         <Link to={'#'} className={"paginate_button page-item previous " + (!hasPrevious() && "disabled")}
                             id="DataTables_Table_0_previous">
-                            <a href="#" aria-controls="DataTables_Table_0" data-dt-idx="0"
-                               tabIndex={0} className="page-link">Previous</a>
+                            <span aria-controls="DataTables_Table_0" data-dt-idx="0"
+                               tabIndex={0} className="page-link">Previous</span>
                         </Link>
                         {Array.from(Array(pagination.totalPages), (e, i) => (
                             <Fragment key={i}>
                                 {(i > currentPage() - 2 && i < currentPage() + 2) &&
                                     <Link to={"#"} className={"paginate_button page-item " + ((currentPage() === i) && "active")}>
-                                        <a href="#" aria-controls="DataTables_Table_0" className="page-link">{i + 1}</a>
+                                        <span aria-controls="DataTables_Table_0" className="page-link">{i + 1}</span>
                                     </Link>
                                 }
                             </Fragment>
                         ))}
                         <Link to={"#"} className={"paginate_button page-item next " + (!hasNext() && "disabled")}
                             id="DataTables_Table_0_next">
-                            <a href="#" aria-controls="DataTables_Table_0" data-dt-idx="3"
-                               tabIndex={0} className="page-link">Next</a>
+                            <span aria-controls="DataTables_Table_0" data-dt-idx="3"
+                               tabIndex={0} className="page-link">Next</span>
                         </Link>
                     </ul>
                 </div>
