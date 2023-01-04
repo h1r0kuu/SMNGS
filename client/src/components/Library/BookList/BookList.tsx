11019c8a6408ff693f60fbe8e09cc18e8f4d020b
h1r0kuu <abhazim.nurbat@gmail.com>
2023-01-04 20:47:40 +0100

    Update

--- client/src/components/Library/BookList/BookList.tsx
+++ client/src/components/Library/BookList/BookList.tsx
@@ -1,45 +1,18 @@
 import {Col, Row} from "react-bootstrap";
 import React from "react";
 import Book from "./Book/Book";
+import {useFetchBooks} from "../../../hooks/library/books/useFetchBooks";
 
 const BookList = () => {
+    const {books, pagination, isLoading} = useFetchBooks()
+
     return (
         <div className={"book-list"}>
             <Col xs={12}>
-                <div className="book-filter">
-                    <div className="btn-group">
-                        <button type="button" className="btn btn-danger dropdown-toggle" data-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">Action
-                        </button>
-                        <div className="dropdown-menu">
-                            <a className="dropdown-item" href="#">Action</a>
-                            <a className="dropdown-item" href="#">Another action</a>
-                            <div className="dropdown-divider"></div>
-                            <a className="dropdown-item" href="#">Separated link</a>
-                        </div>
-                    </div>
-                    <div className="btn-group">
-                        <button type="button" className="btn btn-danger dropdown-toggle" data-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">Action
-                        </button>
-                        <div className="dropdown-menu">
-                            <a className="dropdown-item" href="#">Action</a>
-                            <a className="dropdown-item" href="#">Another action</a>
-                            <div className="dropdown-divider"></div>
-                            <a className="dropdown-item" href="#">Separated link</a>
-                        </div>
-                    </div>
-                </div>
-            </Col>
-            <Col xs={12}>
                 <Row>
-                    <Book/>
-                    <Book/>
-                    <Book/>
-                    <Book/>
-                    <Book/>
-                    <Book/>
-                    <Book/>
+                    {books?.map(book => (
+                        <Book key={book.id} book={book}/>
+                    ))}
                 </Row>
             </Col>
         </div>
--- client/src/pages/Library/Library.tsx
+++ client/src/pages/Library/Library.tsx
@@ -1,5 +1,5 @@
 import Page from "../../components/Page/Page";
-import {BreadcrumbItem, Card, Col, Row} from "react-bootstrap";
+import {BreadcrumbItem, Button, Card, Col, Form, FormControl, FormGroup, Row} from "react-bootstrap";
 import React from "react";
 import BookList from "../../components/Library/BookList/BookList";
 
@@ -15,6 +15,29 @@ const Library = () => {
 
     return (
         <Page title={"Library"} breadcrumbs={breadCrumbs()}>
+            <Form>
+                <FormGroup>
+                    <FormControl
+                        type="text"
+                        placeholder="Search for a book by title"
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <FormControl
+                        type="text"
+                        placeholder="Search for a book by author"
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <FormControl
+                        type="text"
+                        placeholder="Search for a book by publisher"
+                    />
+                </FormGroup>
+                <Button type="submit" variant="primary">
+                    Search
+                </Button>
+            </Form>
             <BookList/>
         </Page>
     )
