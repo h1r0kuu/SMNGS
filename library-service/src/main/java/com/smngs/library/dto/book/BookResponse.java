4e8ab7bab574fb4ff586016a38ac8d26b5f9b604
h1r0kuu <abhazim.nurbat@gmail.com>
2023-01-04 21:12:21 +0100

    Rename

--- library-service/src/main/java/com/smngs/library/dto/book/BookResponse.java
+++ library-service/src/main/java/com/smngs/library/dto/book/BookResponse.java
@@ -1,17 +1,19 @@
 package com.smngs.library.dto.book;
 
 import com.smngs.library.dto.author.AuthorWithoutBooksResponse;
-import com.smngs.library.dto.category.CategoryWithoutBooksResponse;
+import com.smngs.library.dto.genre.GenreWithoutBooksResponse;
 import com.smngs.library.dto.publisher.PublisherWithoutBooksResponse;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Data
 public class BookResponse {
     private Long id;
     private String title;
     private String frontPicture;
-    private CategoryWithoutBooksResponse category;
+    private List<GenreWithoutBooksResponse> genres;
     private PublisherWithoutBooksResponse publisher;
     private List<AuthorWithoutBooksResponse> authors;
     private LocalDateTime createdAt;
