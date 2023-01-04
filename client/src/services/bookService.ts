9f7281ab3450dc3aaebd3af157d30c8745032417
h1r0kuu <abhazim.nurbat@gmail.com>
2023-01-04 20:56:11 +0100

    Create book

--- client/src/services/bookService.ts
+++ client/src/services/bookService.ts
@@ -3,9 +3,17 @@ import {AxiosResponse} from "axios";
 import {axios} from "../config/axios";
 import {API_AUTH_LOGIN, API_LIBRARY_BOOKS} from "../constants/apiConstants";
 import {ResponseWithPagination} from "../types/pagination";
-import {BookResponse} from "../types/book";
+import {BookRequest, BookResponse} from "../types/book";
 
 export const BookService = {
+    async create(data: BookRequest): Promise<AxiosResponse<BookResponse>> {
+        return await axios.post<BookResponse>(API_LIBRARY_BOOKS, data, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            }
+        })
+    },
+
     async findAll(): Promise<AxiosResponse<ResponseWithPagination<BookResponse>>> {
         return await axios.get<ResponseWithPagination<BookResponse>>(API_LIBRARY_BOOKS)
     }
